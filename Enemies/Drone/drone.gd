827065525095383298f8921bbc198eb464f9117e
Youssef Wagih <youssefwagih212@gmail.com>
2024-04-07 13:21:47 +0200

    Drone dies

--- Enemies/Drone/drone.gd
+++ Enemies/Drone/drone.gd
@@ -3,7 +3,9 @@ extends Area2D
 var direction = -1
 var speed = 80
 
-
+@onready var bullet = preload("res://Bullet/bullet.tscn")
+#(30, 10)	(-30, 10)
+@onready var bullet_position = $bullet_position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position.x += speed * delta * direction
@@ -11,3 +13,8 @@ func _process(delta):
 	
 func turn_around():
 	direction *= -1
+
+
+func _on_area_entered(area):
+	if area.is_in_group("Bullet"):
+		queue_free()
